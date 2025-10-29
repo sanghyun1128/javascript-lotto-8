72caccb6dc2773f6d11c9415769c557ae757a76f
sanghyun1128 <sanghyun1128@gmail.com>
2025-10-29 23:41:21 +0900

    test(LottoShopTest): add tests for winning and bonus number validation

--- __tests__/domain/LottoShopTest.js
+++ __tests__/domain/LottoShopTest.js
@@ -1,6 +1,8 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 import LottoShop from '../../src/domain/LottoShop.js';
+import ERROR_MESSAGES from '../../src/consts/errorMessages.js';
+import DEFAULT_VALUES from '../../src/consts/defaultValues.js';
 
 const mockRandoms = (numbers) => {
   MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
@@ -20,4 +22,62 @@ describe('로또 발행 테스트', () => {
 
     expect(lotto.numbers).toEqual(output);
   });
+});
+
+describe('당첨, 보너스 번호 저장 테스트', () => {
+  test('겹치는 값이 있는 경우 에러 발생', () => {
+    const winningNumbers = [2, 1, 3, 5, 6, 4];
+    const bonusNumber = 1;
+
+    expect(() => new LottoShop(winningNumbers, bonusNumber)).toThrow(
+      ERROR_MESSAGES.LOTTO.DUPLICATE_NUMBERS,
+    );
+  });
+
+  test.each([
+    [DEFAULT_VALUES.DOMAIN.LOTTO_NUMBER_COUNT + 1],
+    [DEFAULT_VALUES.DOMAIN.LOTTO_NUMBER_COUNT - 1],
+  ])('당첨번호의 갯수가 정확하지 않을 경우 에러 발생', () => {
+    const winningNumbers = [];
+    let i = 0;
+    for (; i < DEFAULT_VALUES.DOMAIN.LOTTO_NUMBER_COUNT + 1; i += 1) {
+      winningNumbers.push(i + 10);
+    }
+    const bonusNumber = i;
+
+    expect(() => new LottoShop(winningNumbers, bonusNumber)).toThrow(
+      ERROR_MESSAGES.LOTTO.WINNING_NUMBERS_LENGTH,
+    );
+  });
+
+  test('당첨번호의 갯수가 정확할 경우 정상 처리', () => {
+    const winningNumbers = [];
+    let i = 0;
+    for (; i < DEFAULT_VALUES.DOMAIN.LOTTO_NUMBER_COUNT; i += 1) {
+      winningNumbers.push(i + 10);
+    }
+    const bonusNumber = i;
+
+    expect(() => new LottoShop(winningNumbers, bonusNumber)).not.toThrow();
+  });
+
+  test.each([
+    [DEFAULT_VALUES.DOMAIN.MAX_LOTTO_VALUE + 1],
+    [DEFAULT_VALUES.DOMAIN.MIN_LOTTO_VALUE - 1],
+  ])('값이 범위를 벗어날 경우 에러 발생', (bonusNumber) => {
+    const winningNumbers = [2, 1, 3, 5, 6, 4];
+
+    expect(() => new LottoShop(winningNumbers, bonusNumber)).toThrow(
+      ERROR_MESSAGES.LOTTO.NUMBER_RANGE,
+    );
+  });
+
+  test.each([[DEFAULT_VALUES.DOMAIN.MAX_LOTTO_VALUE], [DEFAULT_VALUES.DOMAIN.MIN_LOTTO_VALUE]])(
+    '경계값일 경우 정상 처리',
+    (bonusNumber) => {
+      const winningNumbers = [2, 11, 3, 5, 6, 4];
+
+      expect(() => new LottoShop(winningNumbers, bonusNumber)).not.toThrow();
+    },
+  );
 });
