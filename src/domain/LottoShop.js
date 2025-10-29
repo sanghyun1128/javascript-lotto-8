587a29e5ebd985b44af0f7cf8d9ead75bb9fe8c4
sanghyun1128 <sanghyun1128@gmail.com>
2025-10-29 23:41:36 +0900

    feat(LottoShop): implement validation for winning and bonus numbers

--- src/domain/LottoShop.js
+++ src/domain/LottoShop.js
@@ -2,8 +2,43 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 import Lotto from './Lotto.js';
 import DEFAULT_VALUES from '../consts/defaultValues.js';
+import ERROR_MESSAGES from '../consts/errorMessages.js';
 
 class LottoShop {
+  #winningNumbers;
+
+  #bonusNumber;
+
+  constructor(winningNumbers, bonusNumber) {
+    LottoShop.#validateLottoNumbersLength(winningNumbers.length);
+    winningNumbers.forEach((number) => LottoShop.#validateLottoNumberRange(number));
+    LottoShop.#validateLottoNumberRange(bonusNumber);
+    LottoShop.#validateNoDuplicates([bonusNumber, ...winningNumbers]);
+
+    this.#winningNumbers = winningNumbers;
+    this.#bonusNumber = bonusNumber;
+  }
+
+  static #validateLottoNumbersLength(length) {
+    if (length !== DEFAULT_VALUES.DOMAIN.LOTTO_NUMBER_COUNT)
+      throw new Error(ERROR_MESSAGES.LOTTO.WINNING_NUMBERS_LENGTH);
+  }
+
+  static #validateLottoNumberRange(number) {
+    if (
+      number < DEFAULT_VALUES.DOMAIN.MIN_LOTTO_VALUE ||
+      number > DEFAULT_VALUES.DOMAIN.MAX_LOTTO_VALUE
+    )
+      throw new Error(ERROR_MESSAGES.LOTTO.NUMBER_RANGE);
+  }
+
+  static #validateNoDuplicates(array) {
+    const unique = new Set(array);
+    if (unique.size !== array.length) {
+      throw new Error(ERROR_MESSAGES.LOTTO.DUPLICATE_NUMBERS);
+    }
+  }
+
   static makeLotto() {
     const lottoNumbers = MissionUtils.Random.pickUniqueNumbersInRange(
       DEFAULT_VALUES.DOMAIN.MIN_LOTTO_VALUE,
