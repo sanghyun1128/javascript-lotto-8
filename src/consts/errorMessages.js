bedf4d23e1cecf55d34431b0a6ecb608c187777b
sanghyun1128 <sanghyun1128@gmail.com>
2025-10-29 23:42:12 +0900

    feat(constants): add error messages for lotto number validation

--- src/consts/errorMessages.js
+++ src/consts/errorMessages.js
@@ -5,6 +5,11 @@ const ERROR_MESSAGES = {
     MUST_POSITIVE: '보유 금액은 양수 이어야 합니다.',
     MUST_MULTIPLE_OF_UNIT: `구입 금액은 ${DEFAULT_VALUES.DOMAIN.BALANCE_UNIT}원 단위로 입력해야 합니다.`,
   },
+  LOTTO: {
+    NUMBER_RANGE: `로또의 번호는 ${DEFAULT_VALUES.DOMAIN.MIN_LOTTO_VALUE}-${DEFAULT_VALUES.DOMAIN.MAX_LOTTO_VALUE} 사이어야 합니다`,
+    WINNING_NUMBERS_LENGTH: `당첨 번호의 갯수는 ${DEFAULT_VALUES.DOMAIN.LOTTO_NUMBER_COUNT}개 이어야 합니다.`,
+    DUPLICATE_NUMBERS: '당첨 번호 끼리 겹치면 안됩니다.',
+  },
 };
 
 export default ERROR_MESSAGES;
