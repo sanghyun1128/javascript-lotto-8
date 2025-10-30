35c57780faa5c0dd413ac1f6a01569f85b8bbe51
sanghyun1128 <sanghyun1128@gmail.com>
2025-10-30 21:34:23 +0900

    feat(InputManager): implement getIntegerList function for multiple integer input

--- src/ui/InputManager.js
+++ src/ui/InputManager.js
@@ -1,6 +1,7 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 import ERROR_MESSAGES from '../consts/errorMessages.js';
+import DEFAULT_VALUES from '../consts/defaultValues.js';
 
 class InputManager {
   static async getInteger() {
@@ -9,6 +10,16 @@ class InputManager {
     InputManager.#validateInteger(trimmedInput);
 
     return Number.parseInt(trimmedInput, 10);
+  }
+
+  static async getIntegerList() {
+    const input = await MissionUtils.Console.readLineAsync('');
+    const trimmedInput = input.trim();
+    const integerList = trimmedInput.split(DEFAULT_VALUES.FORMAT.SEPARATOR).map((e) => e.trim());
+
+    integerList.forEach((integer) => InputManager.#validateInteger(integer));
+
+    return integerList.map((integer) => Number.parseInt(integer, 10));
   }
 
   static #validateInteger(integer) {
