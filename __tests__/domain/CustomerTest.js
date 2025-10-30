6f1a50592658d80c6dd8f0ac93b264f628b45b30
sanghyun1128 <sanghyun1128@gmail.com>
2025-10-30 20:25:42 +0900

    test(CustomerTest): add yield calculation tests

--- __tests__/domain/CustomerTest.js
+++ __tests__/domain/CustomerTest.js
@@ -1,6 +1,17 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 import Customer from '../../src/domain/Customer.js';
+import LottoShop from '../../src/domain/LottoShop.js';
 import DEFAULT_VALUES from '../../src/consts/defaultValues.js';
 import ERROR_MESSAGES from '../../src/consts/errorMessages.js';
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce(
+    (acc, number) => acc.mockReturnValueOnce(number),
+    MissionUtils.Random.pickUniqueNumbersInRange,
+  );
+};
 
 describe('고객 잔액 저장 테스트', () => {
   test.each([
@@ -23,5 +34,38 @@ describe('고객 잔액 저장 테스트', () => {
     const input = DEFAULT_VALUES.DOMAIN.BALANCE_UNIT * 1.5;
 
     expect(() => new Customer(input)).toThrow(ERROR_MESSAGES.BALANCE.MUST_MULTIPLE_OF_UNIT);
+  });
+});
+
+describe('수익률 계산 테스트', () => {
+  test('보유한 로또가 없을 경우 에러 발생', () => {
+    const customer = new Customer(DEFAULT_VALUES.DOMAIN.BALANCE_UNIT);
+
+    expect(() => customer.calculateYield()).toThrow(ERROR_MESSAGES.LOTTO.NO_OWNED_LOTTO);
+  });
+
+  test('결과가 없을 경우 에러 발생', () => {
+    const customer = new Customer(DEFAULT_VALUES.DOMAIN.BALANCE_UNIT);
+    const shop = new LottoShop();
+    shop.buyLotto(customer);
+
+    expect(() => customer.calculateYield()).toThrow(ERROR_MESSAGES.LOTTO.MUST_CHECK_RESULTS);
+  });
+
+  test.each([
+    [[1, 2, 3, 4, 5, 6], (DEFAULT_VALUES.PRIZE.FIRST / DEFAULT_VALUES.DOMAIN.LOTTO_PRICE) * 100],
+    [[1, 2, 3, 11, 12, 13], (DEFAULT_VALUES.PRIZE.FIFTH / DEFAULT_VALUES.DOMAIN.LOTTO_PRICE) * 100],
+  ])('정상 동작', (lottoNumbers, expectYield) => {
+    const customer = new Customer(DEFAULT_VALUES.DOMAIN.BALANCE_UNIT * 2);
+    const shop = new LottoShop();
+    shop.winningNumbers = [1, 2, 3, 4, 5, 6];
+    shop.bonusNumber = 7;
+    mockRandoms([lottoNumbers, lottoNumbers]);
+    shop.buyLotto(customer);
+    shop.buyLotto(customer);
+    customer.checkLottoResults(shop);
+
+    const yieldValue = customer.calculateYield();
+    expect(yieldValue).toBe(expectYield);
   });
 });
