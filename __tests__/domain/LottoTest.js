17aa682eb0021d8d9ac32ca686195d0db13b72f8
sanghyun1128 <sanghyun1128@gmail.com>
2025-10-30 20:53:26 +0900

    test(LottoTest): add validation test
    - number range validation
    - numbers sort ascending validation

--- __tests__/domain/LottoTest.js
+++ __tests__/domain/LottoTest.js
@@ -1,18 +1,23 @@
+import DEFAULT_VALUES from '../../src/consts/defaultValues.js';
+import ERROR_MESSAGES from '../../src/consts/errorMessages.js';
 import Lotto from '../../src/domain/Lotto.js';
 
 describe('로또 클래스 테스트', () => {
-  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow('[ERROR]');
+  test('로또 번호의 개수가 6개가 넘어가면 에러 발생', () => {
+    expect(() => new Lotto([1, 2, 3, 4, 5, 6, 7])).toThrow(ERROR_MESSAGES.LOTTO.NUMBERS_LENGTH);
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
-  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow('[ERROR]');
+  test('로또 번호에 중복된 숫자가 있으면 에러 발생', () => {
+    expect(() => new Lotto([1, 2, 3, 4, 5, 5])).toThrow(ERROR_MESSAGES.LOTTO.DUPLICATE_NUMBERS);
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test('로또 번호에 범위를 벗어난 숫자가 있으면 에러 발생', () => {
+    expect(() => new Lotto([1, 2, 3, 4, 5, DEFAULT_VALUES.DOMAIN.MAX_LOTTO_VALUE + 1])).toThrow(
+      ERROR_MESSAGES.LOTTO.NUMBER_RANGE,
+    );
+  });
+
+  test('로또 번호가 정렬 되어 있지 않으면 에러 발생', () => {
+    expect(() => new Lotto([1, 2, 6, 4, 5, 3])).toThrow(ERROR_MESSAGES.LOTTO.MUST_ORDER_ASC);
+  });
 });
