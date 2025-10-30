f941262c641d4a2d4dc0f4681bdc800f2fd8fccb
sanghyun1128 <sanghyun1128@gmail.com>
2025-10-30 20:55:17 +0900

    feat(Lotto): add validation logic
    - validate no duplicate in numbers
    - validate all numbers are in range
    - validate numbers are ordered ascending

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,8 +1,15 @@
+import DEFAULT_VALUES from '../consts/defaultValues.js';
+import ERROR_MESSAGES from '../consts/errorMessages.js';
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.#validate(numbers);
+    Lotto.#validateNumbersLength(numbers);
+    Lotto.#validateNoDuplicate(numbers);
+    numbers.forEach((number) => Lotto.#validateNumberRange(number));
+    Lotto.#validateIsOrderASC(numbers);
+
     this.#numbers = numbers;
   }
 
@@ -10,13 +17,34 @@ class Lotto {
     return this.#numbers;
   }
 
-  #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+  static #validateNumbersLength(numbers) {
+    if (numbers.length !== DEFAULT_VALUES.DOMAIN.LOTTO_NUMBER_COUNT) {
+      throw new Error(ERROR_MESSAGES.LOTTO.NUMBERS_LENGTH);
     }
   }
 
-  // TODO: 추가 기능 구현
+  static #validateNoDuplicate(numbers) {
+    const unique = new Set(numbers);
+    if (unique.size !== numbers.length) {
+      throw new Error(ERROR_MESSAGES.LOTTO.DUPLICATE_NUMBERS);
+    }
+  }
+
+  static #validateNumberRange(number) {
+    if (
+      number < DEFAULT_VALUES.DOMAIN.MIN_LOTTO_VALUE ||
+      number > DEFAULT_VALUES.DOMAIN.MAX_LOTTO_VALUE
+    )
+      throw new Error(ERROR_MESSAGES.LOTTO.NUMBER_RANGE);
+  }
+
+  static #validateIsOrderASC(numbers) {
+    for (let i = 1; i < numbers.length; i += 1) {
+      if (numbers[i] < numbers[i - 1]) {
+        throw new Error(ERROR_MESSAGES.LOTTO.MUST_ORDER_ASC);
+      }
+    }
+  }
 }
 
 export default Lotto;
