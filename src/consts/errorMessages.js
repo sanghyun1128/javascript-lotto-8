1004cc555984cc01b4a3c6d234f1fb5224f4be2a
sanghyun1128 <sanghyun1128@gmail.com>
2025-10-30 16:06:41 +0900

    fix(constants): correct error message for balance input requirement

--- src/consts/errorMessages.js
+++ src/consts/errorMessages.js
@@ -3,7 +3,7 @@ import DEFAULT_VALUES from './defaultValues.js';
 const ERROR_MESSAGES = {
   BALANCE: {
     MUST_POSITIVE: '보유 금액은 양수 이어야 합니다.',
-    MUST_MULTIPLE_OF_UNIT: `구입 금액은 ${DEFAULT_VALUES.DOMAIN.BALANCE_UNIT}원 단위로 입력해야 합니다.`,
+    MUST_MULTIPLE_OF_UNIT: `보유 금액은 ${DEFAULT_VALUES.DOMAIN.BALANCE_UNIT}원 단위로 입력해야 합니다.`,
   },
   LOTTO: {
     NUMBER_RANGE: `로또의 번호는 ${DEFAULT_VALUES.DOMAIN.MIN_LOTTO_VALUE}-${DEFAULT_VALUES.DOMAIN.MAX_LOTTO_VALUE} 사이어야 합니다`,
