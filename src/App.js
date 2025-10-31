bd9f956e878783875c2cbeb992323c206a46601c
sanghyun1128 <sanghyun1128@gmail.com>
2025-10-31 21:50:44 +0900

    feat(App, Controller): implement main application logic and controller functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import OutputManager from './ui/OutputManager.js';
+import Controller from './Controller.js';
+
 class App {
-  async run() {}
+  #controller;
+
+  constructor() {
+    this.#controller = new Controller();
+  }
+
+  async run() {
+    try {
+      await this.#controller.requestBalance();
+      this.#controller.buyLottosAndPrint();
+      await this.#controller.requestWinningNumbers();
+      await this.#controller.requestBonusNumber();
+      this.#controller.checkResultsAndPrint();
+    } catch (error) {
+      error.message = OutputManager.applyErrorMessageFormat(error.message);
+      throw error;
+    }
+  }
 }
 
 export default App;
---
+++ src/Controller.js
@@ -0,0 +1,63 @@
+import OutputManager from './ui/OutputManager.js';
+import InputManager from './ui/InputManager.js';
+import Customer from './domain/Customer.js';
+import LottoShop from './domain/LottoShop.js';
+
+class Controller {
+  #customer;
+
+  #shop;
+
+  constructor() {
+    this.#customer = null;
+    this.#shop = new LottoShop();
+  }
+
+  async requestBalance() {
+    OutputManager.printRequestBalance();
+    const balance = await InputManager.getInteger();
+
+    this.#customer = new Customer(balance);
+  }
+
+  buyLottosAndPrint() {
+    while (this.#customer.balance > 0) {
+      this.#shop.buyLotto(this.#customer);
+    }
+
+    OutputManager.printEmptyLine();
+    OutputManager.printBoughtLottosCount(this.#customer.ownedLottos.length);
+    this.#customer.ownedLottos.forEach((lotto) => OutputManager.printLottoNumbers(lotto.numbers));
+  }
+
+  async requestWinningNumbers() {
+    OutputManager.printEmptyLine();
+    OutputManager.printRequestWinningNumbers();
+    const winningNumbers = await InputManager.getIntegerList();
+
+    this.#shop.winningNumbers = winningNumbers;
+  }
+
+  async requestBonusNumber() {
+    OutputManager.printEmptyLine();
+    OutputManager.printRequestBonusNumber();
+    const bonusNumber = await InputManager.getInteger();
+
+    this.#shop.bonusNumber = bonusNumber;
+  }
+
+  checkResultsAndPrint() {
+    this.#customer.checkLottoResults(this.#shop);
+
+    OutputManager.printEmptyLine();
+    OutputManager.printResultHeader();
+    OutputManager.printResult(this.#customer.lottoResults.fifth);
+    OutputManager.printResult(this.#customer.lottoResults.fourth);
+    OutputManager.printResult(this.#customer.lottoResults.third);
+    OutputManager.printResult(this.#customer.lottoResults.second);
+    OutputManager.printResult(this.#customer.lottoResults.first);
+    OutputManager.printYield(this.#customer.calculateYield());
+  }
+}
+
+export default Controller;
